847272f81ee91331c2cd0ccb2a3a283eb6411d86
KhosraviEngineer <khosravi.jsdev@gmail.com>
2025-05-16 14:03:19 +0330

    Add final result

--- dual-channel Scripts.R
+++ dual-channel Scripts.R
@@ -16,7 +16,6 @@ BiocManager::install("limma")
 # Set your working directory where raw files are saved
 setwd("C:/Users/khosravi/OneDrive/Desktop/MetaAnalysis_khosravi/Microarray_dual-channel/microarray-dual-channel-analysis")
 library(limma)
-setwd("./GSE115800_T24samples_RAW")
 #prepare targets file manually
 targets <- read.delim("targets.txt", sep = "\t")
 head(targets)
@@ -25,25 +24,24 @@ RG <- read.maimages(files = targets$FileName,
                     source = "agilent",
                     green.only = FALSE)
 #########################################
-#QC & Normalization
+#QC & Normalization:
 #########################################
 #QC:
 boxplot(RG$R, main = "Raw Red Channel", las = 2)
 boxplot(RG$G, main = "Raw Green Channel", las = 2)
 #background correction:
 RGb <- backgroundCorrect(RG, method = "normexp")
-MA <- normalizeWithinArrays(RGb, method = "loess")
-
 #normalization:
-#MA <- normalizeWithinArrays(RG.bg, method = "loess")
-#MA <- normalizeBetweenArrays(MA, method = "Aquantile")
-
-
+MA <- normalizeWithinArrays(RGb, method = "loess")
 #########################################
-#Analysis
+#Analysis:
 #########################################
 fit <- lmFit(MA)
 fit <- eBayes(fit)
 top_genes <- topTable(fit, number = 50)
+
+#Final result:
 write.csv(top_genes, file = "top_genes.csv", row.names = TRUE)
+
+
 
